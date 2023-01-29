0903c34ba14499137b6d2b9539ec13e4448536d5
Andrii Maliuta <quadr988@gmail.com>
2023-01-29 21:03:08 +0200

    http

--- main/main.go
+++ main/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
 	"fmt"
 	_ "github.com/jackc/pgx"
 	_ "github.com/lib/pq"
+	"golang.org/x/net/http2"
 	"io"
 	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
 	"time"
 )
+
+var httpVersion = flag.Int("version", 2, "HTTP version")
 
 func main() {
 	start := time.Now()
@@ -30,13 +37,64 @@ func checkError(err error) {
 	}
 }
 
+func GetH2(url string) {
+	flag.Parse()
+	client := &http.Client{}
+
+	// Create a pool with the server certificate since it is not signed
+	// by a known CA
+	caCert, err := ioutil.ReadFile("server.crt")
+	if err != nil {
+		log.Fatalf("Reading server certificate: %s", err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Create TLS configuration with the certificate of the server
+	tlsConfig := &tls.Config{
+		RootCAs: caCertPool,
+	}
+
+	// Use the proper transport in the client
+	switch *httpVersion {
+	case 1:
+		client.Transport = &http.Transport{
+			TLSClientConfig: tlsConfig,
+		}
+	case 2:
+		client.Transport = &http2.Transport{
+			TLSClientConfig: tlsConfig,
+		}
+	}
+
+	// Perform the request
+	resp, err := client.Get(url)
+	if err != nil {
+		log.Fatalf("Failed get: %s", err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed reading response body: %s", err)
+	}
+	fmt.Printf(
+		"Got response %d: %s %s\n",
+		resp.StatusCode, resp.Proto, string(body))
+}
+
 func post() {
 	con, err := net.Dial("tcp", "httpbin.org:80")
 	checkError(err)
 
 	req := "POST /post HTTP/1.1\r\n" +
 		"Host: httpbin.org\r\n" +
-		"test body"
+		"Content-Type: application/json\r\n\r\n" +
+		"test body\r\n"
 
 	_, err = con.Write([]byte(req))
 	checkError(err)
